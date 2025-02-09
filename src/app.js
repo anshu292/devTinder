8ff1ee0923bd66ee40c26d4b2968187606be197a
Anshu <anshugupta29a@gmail.com>
2025-02-09 21:46:24 +0530

    the order of using use and get matters

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express= require('express')
 
 const app= express();
 
-//this will only handle the GET call to /user
+//all the get , delete, post will match this one
+app.use("/test", (req, res) =>{
+    res.send("Hello from the test server");
+});
+
 app.get("/user", (req, res) =>{
     res.send({firstName :"Anshu", lastName:"Gupta"});
 });
 
-//this will match all the HTTP method API calls to /test
 
-app.use("/test", (req, res) =>{
-    res.send("Hello from the test server");
+app.post("/user", (req, res) =>{
+    res.send("Data saved successfully to the database!");
+
 });
+
 app.listen(7777, () =>{
 console.log("Server is successfully listening on port 7777...");
 });
