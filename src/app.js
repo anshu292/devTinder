497d7713142b100149798d8eeb6e394dd9a1f98e
Anshu <anshugupta29a@gmail.com>
2025-02-09 22:19:30 +0530

    use of : for dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,21 +2,11 @@ const express= require('express')
 
 const app= express();
 
-//all the get , delete, post will match this one
-app.use("/test", (req, res) =>{
-    res.send("Hello from the test server");
+//http://localhost:7777/user/707/Akshay/testing
+app.get("/user/:userId/:name/:password", (req, res) =>{
+    console.log(req.params);
+    res.send({ firstName :"Anshu", lastName: "Gupta"});
 });
-
-app.get("/user", (req, res) =>{
-    res.send({firstName :"Anshu", lastName:"Gupta"});
-});
-
-
-app.post("/user", (req, res) =>{
-    res.send("Data saved successfully to the database!");
-
-});
-
 app.listen(7777, () =>{
 console.log("Server is successfully listening on port 7777...");
 });
