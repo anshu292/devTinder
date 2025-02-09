cf644b2a6f868a7111d86e20f806df06f26d47b3
Anshu <anshugupta29a@gmail.com>
2025-02-09 21:40:52 +0530

    get and use difference

--- src/app.js
+++ src/app.js
@@ -2,29 +2,16 @@ const express= require('express')
 
 const app= express();
 
-
-app.use("/hello/7/7", (req, res) =>{
-    res.send("Hello from /7/7");
-
+//this will only handle the GET call to /user
+app.get("/user", (req, res) =>{
+    res.send({firstName :"Anshu", lastName:"Gupta"});
 });
-app.use("/hello/7", (req, res)=>{
-    res.send("Hello from /hello/7");
 
-});
-app.use("/hello", (req, res) =>{
+//this will match all the HTTP method API calls to /test
 
-    res.send("Hello from the /hello");
-
-});
 app.use("/test", (req, res) =>{
     res.send("Hello from the test server");
 });
-
-app.use("/", (req, res) =>{
-    res.send("Hello from the /");
-});
-
-
 app.listen(7777, () =>{
 console.log("Server is successfully listening on port 7777...");
 });
